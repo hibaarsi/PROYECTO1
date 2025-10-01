a4b034fe45a4eb1f439ca94778b903fa13ca4d50
hibaarsi <hiba.arsi@alumnos.upm.es>
2025-10-02 00:32:05 +0200

    addproduct y newticket en ticket

--- src/main/java/etsisi/poo/Ticket.java
+++ src/main/java/etsisi/poo/Ticket.java
@@ -1,16 +1,36 @@
 package etsisi.poo;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Ticket {
     private ArrayList<Product> productos;
+    private Catalogo catalogo;
     private static final int MAX_PRODUCTOS=100;
 
-    public Ticket() {
+    public Ticket(Catalogo catalogo) {
         this.productos = new ArrayList<>();
+        this.catalogo = catalogo;
     }
-   public void addProduct(Product product,int cantidad){
- //hola
+    public void newTicket(){
+        productos.clear();
+    }
+   public void addProduct(int productId,int cant){
+        if(catalogo.existProduct(productId)){
+            throw new IllegalArgumentException("Product with id " + productId + " does not exist");
+        }
+        if(productos.size() + cant >=MAX_PRODUCTOS)
+            throw new IllegalArgumentException("You cant add more products");
+        Product product = catalogo.getProduct(productId);
+        for(int i=0;i<cant; i++) {
+            productos.add(product);
+        }
    }
+   public void removeProduct(int productId){
+        int contador=0;
+        // comprobar por id y quitar todas las cantidades
+   }
+   //importe provisional, imprimir tickrt, updateticket,
+
 
 }
