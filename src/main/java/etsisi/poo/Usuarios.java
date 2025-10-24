dad3cd9dda17f187f1749439b9d920b83084145c
luci <lucila.peralta@alumnos.upm.es>
2025-10-24 10:34:07 +0200

    usuarios haciendoo

--- src/main/java/etsisi/poo/Usuarios.java
+++ src/main/java/etsisi/poo/Usuarios.java
@@ -8,7 +8,7 @@ public class Usuarios {
     public Usuarios(String name, String email) {
         this.name = name;
         this.email = email;
-    }
+    }//
 
     public String getName() {
         return name; }
