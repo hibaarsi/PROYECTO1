9073f225baa3997b62c188395264ebe5ccf4e8db
luci <lucila.peralta@alumnos.upm.es>
2025-11-09 13:25:25 +0100

    terminé el productReunion

--- src/main/java/etsisi/poo/ProductReunion.java
+++ src/main/java/etsisi/poo/ProductReunion.java
@@ -2,9 +2,9 @@ package etsisi.poo;
 import java.time.*;
 
 public class ProductReunion extends Product implements EventProduct {
-    private final LocalDateTime eventDate;
+    private final LocalDateTime eventDate; //cuando es la reunion
     private final int maxPeople;
-    private static final Duration MIN_PLANNING = Duration.ofHours(12);
+    private static final Duration MIN_PLANNING = Duration.ofHours(12); //tiempo min de antelacion para programarla
 
     public ProductReunion(int id, String name, double price, LocalDateTime eventDate, int maxPeople) {
         super(id, name, null, price);
@@ -12,8 +12,8 @@ public class ProductReunion extends Product implements EventProduct {
             throw new IllegalArgumentException("maxPeople must be between 1 and 100");
         this.eventDate = eventDate;
         this.maxPeople = maxPeople;
-        if (!hasEnoughPlanning()) {
-            System.out.println("Warning: MeetingProduct does not meet 12-hour planning rule");
+        if (!hasEnoughPlanning()) { //comprara el tiempo que falta entre LocalDateTime.now() y eventdate
+            System.out.println("Warning: MeetingProduct does not meet 12-hour planning rule"); //si se pasa de 12h
         }
     }
 
