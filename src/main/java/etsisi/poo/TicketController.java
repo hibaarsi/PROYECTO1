7e380879fd6da8dd8629bedbe6b8288d21ece20b
hibaarsi <hiba.arsi@alumnos.upm.es>
2025-11-09 14:53:30 +0100

    ticketmodel y controller

--- src/main/java/etsisi/poo/TicketController.java
+++ src/main/java/etsisi/poo/TicketController.java
@@ -1,4 +1,15 @@
 package etsisi.poo;
 
 public class TicketController {
+    private TicketModel ticketModel;
+    private Catalog catalog;
+    private UserController users;
+
+    public TicketController(TicketModel ticketModel, Catalog catalog, UserController user) {
+        this.ticketModel = ticketModel;
+        this.catalog = catalog;
+        this.users = user;
+
+    }
+
 }
