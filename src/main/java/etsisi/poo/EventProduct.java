cf1eeaa47387ed55dd38d62d95bbbaaf8fdfb6c0
Alina Souto Russo <alina.souto@alumnos.upm.es>
2025-11-09 12:57:21 +0100

    He creado una interfaz para poder usar en las comidas y reuniones para saber si el tiempo de planificación es suficiente. Y he completado la clase ProductFood

---
+++ src/main/java/etsisi/poo/EventProduct.java
@@ -0,0 +1,14 @@
+package etsisi.poo;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public interface EventProduct {
+    LocalDateTime getEventDate();
+    int getMaxPeople();
+    Duration getMinPlanning(); //tiempo mínimo de planificación
+
+    default boolean hasEnoughPlanning() { //para ver si hay suficiente tiempo de planificación
+        return Duration.between(LocalDateTime.now(), getEventDate()).compareTo(getMinPlanning()) >= 0;
+    }
+}
--- src/main/java/etsisi/poo/ProductFood.java
+++ src/main/java/etsisi/poo/ProductFood.java
@@ -1,4 +1,30 @@
 package etsisi.poo;
 
-public class ProductFood {
+import java.time.*;
+
+public class ProductFood extends Product implements EventProduct {
+
+    private final LocalDateTime eventDate; //fecha del evento
+    private final int maxPeople;
+    private static final Duration MIN_PLANNING = Duration.ofDays(3); //el tiempo minimo de planificación son 3 dias
+
+    public ProductFood(int id, String name, double price, LocalDateTime eventDate, int maxPeople) {
+        super(id, name, null, price); // sin categoría
+        if (maxPeople < 1 || maxPeople > 100)
+            throw new IllegalArgumentException("The capacity of people is 100");
+        this.eventDate = eventDate;
+        this.maxPeople = maxPeople;
+        if (!hasEnoughPlanning()) {
+            System.out.println("Warning: FoodProduct does not meet 3-day planning rule");
+        }
+    }
+
+    @Override
+    public LocalDateTime getEventDate() { return eventDate; }
+
+    @Override
+    public int getMaxPeople() { return maxPeople; }
+
+    @Override
+    public Duration getMinPlanning() { return MIN_PLANNING; }
 }
