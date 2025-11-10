830d6dc8e2e2eba22abcb07c2144f48a48f243ee
daniela <manycloudzs@gmail.com>
2025-11-10 21:51:27 +0100

    he añadido getName y getSurname en ICommand para que pueda leer la primera palabra del comando y la segunda.

--- src/main/java/etsisi/poo/Commands/ICommand.java
+++ src/main/java/etsisi/poo/Commands/ICommand.java
@@ -2,5 +2,6 @@ package etsisi.poo.Commands;
 
 public interface ICommand {
     String execute(String[] args);
-
+    String getName();//la primera parte del comando
+    String getSubName();// la segunda parte del comando
 }
